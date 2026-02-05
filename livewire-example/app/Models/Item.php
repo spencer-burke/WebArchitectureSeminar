fbc7e49cbe5c4209a6774c7f513c6e8ca66be46a
spencer burke <info.spencer.burke@gmail.com>
2026-02-05 15:43:59 -0800

    Add more of the livewire example

--- livewire-example/app/Models/Item.php
+++ livewire-example/app/Models/Item.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+       'name',
+       'description'
+    ];
+
+    protected $casts = [
+        'name' => 'string',
+        'description' => 'string'
+    ];
 }
--- livewire-example/resources/views/components/landing.blade.php
+++ livewire-example/resources/views/components/landing.blade.php
@@ -4,12 +4,17 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public function setItems(): void
+    {
+
+    }
 };
 ?>
 
 <div>
     <main class="container">
+        <!-- Item Form -->
 
+        <!-- Item List -->
     </main>
 </div>
