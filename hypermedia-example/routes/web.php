08ecd435531e7b3c884dca222c713803c7b2718b
spencer burke <info.spencer.burke@gmail.com>
2026-02-07 23:17:06 -0800

    Change case sensitivity for views

--- hypermedia-example/routes/web.php
+++ hypermedia-example/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landing');
+    return view('Landing');
 });
 
 Route::get('/items', function () {
-    return view('_items');
+    return view('_Items');
 })->name('items');
